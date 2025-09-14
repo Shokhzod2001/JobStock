4533bef75856353fcf0309886a20e601c7b03a53
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-09-14 17:26:16 +0900

    fix: fix noticeCategory enum

--- apps/jobStock-api/src/libs/enums/notice.enum.ts
+++ apps/jobStock-api/src/libs/enums/notice.enum.ts
@@ -3,7 +3,7 @@ import { registerEnumType } from '@nestjs/graphql';
 export enum NoticeCategory {
 	FAQ = 'FAQ',
 	TERMS = 'TERMS',
-	INQUIRY = 'NOTICE',
+	NOTICE = 'NOTICE',
 }
 registerEnumType(NoticeCategory, {
 	name: 'NoticeCategory',
